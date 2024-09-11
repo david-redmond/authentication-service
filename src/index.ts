9ccfb3f88c35a99d129e82b5b7cef8666357e9da
david-redmond <dredmond@wynshop.com>
2024-09-12 00:42:10 +0100

    using FB and jwts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,47 @@
 import * as express from "express";
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
-import * as dotenv from "dotenv";
+import { config } from "dotenv";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
-import createNewUser from "./API/createNewUser";
-import checkUserByEmail from "./API/checkUserByEmail";
-import { IUser } from "./API/interfaces";
-dotenv.config();
+import { IUser } from "./models/User";
+import { mongoConnection } from "./database/connection";
+import { User } from "./models/User";
+
+config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-const secretKey = process.env.SECRET_KEY || "---";
 
 app.use(bodyParser.json());
 app.use(cors());
+mongoConnection();
 
 // Register endpoint
-app.post("/register", async (req, res) => {
+app.post("/auth/register", async (req, res) => {
   try {
     const { firstname, surname, email, password } = req.body;
-    // Check if user already exists
-    const existingUser: IUser | null = await checkUserByEmail(email);
-    if (!!existingUser) {
-      return res.status(400).send("User already exists.");
+
+    const user: IUser | null = await User.findOne({ email: req.body.email });
+    if (!!user) {
+      console.error(
+        "Error POST /register : user already exists",
+        req.body.email,
+      );
+      return res.status(403).send("Forbidden");
     } else {
       // Hash password
       const hashedPassword = await bcrypt.hash(password, 10);
       // Create new user
-      await createNewUser({
-        firstname,
-        surname,
-        email,
-        hashedPassword,
-        attributes: {}
+      // Create new user
+      const newUser = new User({
+        firstname: firstname,
+        surname: surname,
+        email: email,
+        password: hashedPassword,
+        attributes: {},
       });
+      await newUser.save();
       return res.status(201).send("User registered successfully.");
     }
   } catch (error) {
@@ -42,25 +50,25 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
+app.post("/auth/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    // Find user by email
-    const user: IUser = await checkUserByEmail(email);
+
+    const user: IUser | null = await User.findOne({ email });
     if (!user) {
-      console.log("POST /login: user doesn't exist.");
+      console.error("POST /auth/login : user doesn't exist", req.body.email);
       return res.status(400).send("Invalid email or password.");
     }
 
     // Check password
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      console.log("POST /login: Invalid password.");
+      console.log("POST /auth/login: Invalid password.");
       return res.status(400).send("Invalid email or password.");
     }
 
     // Generate JWT token
-    const token = jwt.sign({ _id: user._id }, secretKey);
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
     res
       .header("x-auth-token", token)
       .header("access-control-expose-headers", "x-auth-token")
@@ -73,6 +81,46 @@ app.post("/login", async (req, res) => {
       error.config,
     );
     res.status(500).send("Server error.");
+  }
+});
+
+app.post("/auth/facebook", async (req, res) => {
+  const { email, name, picture } = req.body;
+
+  try {
+    // Check if user already exists
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      // If user doesn't exist, create a new user
+      const splitname = name.split(" ");
+      const newUser = {
+        firstname: splitname[0] || "Firstname",
+        surname: splitname[1] || "Surname",
+        email,
+        provider: "facebook",
+        attributes: {
+          picture,
+        },
+      };
+      user = new User(newUser);
+      await user.save();
+    } else {
+      await user.save();
+    }
+
+    // Generate JWT token
+    const {_id, firstname, surname, provider } = user;
+    const token = jwt.sign({ _id, firstname, surname, email, provider}, process.env.SECRET_KEY);
+
+    // Send the token to the client
+    res
+      .header("x-auth-token", token)
+      .header("access-control-expose-headers", "x-auth-token")
+      .json({ message: "Login successful.", user });
+  } catch (error) {
+    console.error("Error during Facebook login:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { Model, Schema, model } from "mongoose";
+
+export interface IUser {
+  _id: string;
+  firstname: string;
+  surname: string;
+  email: string;
+  password?: string;
+  provider: string;
+  attributes: { [key: string]: any };
+}
+
+interface UserModel extends Model<IUser> {
+  // myStaticMethod(): number;
+}
+
+const userSchema = new Schema<IUser, UserModel>({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  provider: {
+    type: String,
+    required: true
+  },
+  attributes: {
+    type: Object,
+    required: true
+  },
+}, { minimize: false });
+
+const User = model<IUser, UserModel>("Users", userSchema);
+
+export { User };
